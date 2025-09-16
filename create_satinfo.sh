d4a980759a03343dfe45baa5631dd689b850fdd4
Jeffrey Whitaker <jwhitaker@Jeffreys-MacBook-Air.local>
2025-09-15 19:25:52 -0600

    update

--- create_satinfo.sh
+++ create_satinfo.sh
@@ -1,25 +1,27 @@
+#! /usr/bin/env bash
 # create global_satinfo file for a given date
-date=$1
+date_in=$1
 # header
 echo '!sensor/instr/sat      chan iuse  error  error_cld  ermax   var_b    var_pg  icld_det icloud iaerosol'
 # loop over satellites
-cd satinfo
-for sat in $(cat satellites); do
+cd satinfo || exit 1
+while IFS= read -r sat 
+do
     # find matching date
     usedate=""
-    for f in $sat/*; do
-        if [ $f != "$sat/readme" ]; then # skip readme file
-           datex=`basename $f`
-	   if [ $date -ge $datex ]; then
-              usedate=$datex
+    for f in "${sat}"/*; do
+        if [[ ${f} != "${sat}/readme" ]]; then # skip readme file
+           datex=$(basename "${f}")
+           if [[ ${date_in} -ge ${datex} ]]; then
+              usedate=${datex}
            fi
         fi
     done
     # cat matching date file, or quit if date not found
-    if [ $usedate != "" ]; then
-        cat $sat/$usedate
+    if [[ ${usedate} != "" ]]; then
+        cat "${sat}/${usedate}" || exit 1
     else
-        echo "date not found for $sat"
-	exit 1
+        echo "date not found for ${sat}"
+        exit 1
     fi
-done
+done < <(grep -v '^ *#' satellites)
